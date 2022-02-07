4cd289a8afc2956ed14d5466f31c5226f5a06408
raveneau <emilien.raveneau@gmail.com>
2022-02-07 14:33:18 +0100

    fusion des fonctions

--- tp3/Main.java
+++ tp3/Main.java
@@ -6,27 +6,33 @@ import static java.lang.Integer.parseInt;
 public class Main {
   public static void main(String[] args) {
     List<String> list = FilesOp.readFile(args[0]);
-    //System.out.println(list);
+    // System.out.println(list);
     int nbMots = parseInt(list.get(0));
     String chaineLongue = list.get(nbMots+1);
-    System.out.println(chaineLongue);
+    // System.out.println(chaineLongue);
+    System.out.println(reserch(chaineLongue, motsARechercher(list)));
 
   }
 
-  public String[] motsARechercher (String[] texte){
-     int n = parseInt(texte[0]);
+  public static String[] motsARechercher (List<String> texte){
+     int n = parseInt(texte.get(0));
      String[] res = new String [n];
-     for(int i = 0; i < n; i++ ){
-       res[i] = texte[i+1];
+     // for(int i = 0; i < n; i++ ){
+     int i=0;
+     for (String s : texte) {
+       if(i>0 && i<n){
+         res[i-1] = s;
+       }
+       i++;
      }
      return res;
   }
-  public List<List<Integer>> reserch(String chaineLongue, String mots[]){
+  public static List<List<Integer>> reserch(String chaineLongue, String mots[]){
     List<List<Integer>> listOfList = new ArrayList<List<Integer>>();
     for (String mot : mots) {
       listOfList.add(makeIntList(chaineLongue, mot));
     }
     return listOfList;
   }
-  public List<Integer> makeIntList(String s, String s2){return null;}
+  public static List<Integer> makeIntList(String s, String s2){return new ArrayList<Integer>();}
 }
