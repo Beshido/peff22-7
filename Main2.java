3e93b93b94d54ac645fb96d4b54318093e887128
HAMIMI dany <dany.hamimi@gmail.com>
2022-02-07 14:59:41 +0100

    Update Main2.java, FilesOp2.java files

---
+++ Main2.java
@@ -0,0 +1,47 @@
+import java.util.List;
+import java.util.ArrayList;
+
+import static java.lang.Integer.parseInt;
+
+public class Main2 {
+  public static void main(String[] args) {
+    List<String> list = FilesOp2.readFile(args[0]);
+    // System.out.println(list);
+    int nbMots = parseInt(list.get(0));
+    System.out.println(list);
+
+  }
+
+  public static String[] motsARechercher (List<String> texte){
+     int n = parseInt(texte.get(0));
+     String[] res = new String [n];
+     // for(int i = 0; i < n; i++ ){
+     int i=0;
+     for (String s : texte) {
+       if(i>0 && i<=n){
+         res[i-1] = s;
+       }
+       i++;
+     }
+     return res;
+  }
+  public static List<List<Integer>> reserch(String chaineLongue, String mots[]){
+    List<List<Integer>> listOfList = new ArrayList<List<Integer>>();
+    for (String mot : mots) {
+      listOfList.add(makeIntList(chaineLongue, mot));
+    }
+    return listOfList;
+  }
+    public static ArrayList<Integer> makeIntList(String texteSeul, String mot){
+        ArrayList<Integer> res = new ArrayList();
+        System.out.println(mot);
+        if(mot==null){return res;}
+        int a = texteSeul.indexOf(mot);
+        while(a < texteSeul.length() && a != -1){
+            res.add(a);
+            a = texteSeul.indexOf(mot, a+ mot.length());
+        }
+        return res;
+    }
+
+}
