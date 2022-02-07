1345c5793add52eca04121a4e9f401fe0fa713e6
raveneau <emilien.raveneau@gmail.com>
2022-02-07 14:17:04 +0100

    Add reserch(...) for all mots.

--- tp3/Main.java
+++ tp3/Main.java
@@ -1,8 +1,17 @@
 import java.util.List;
+import java.util.ArrayList;
 
 public class Main {
   public static void main(String[] args) {
     List<String> list = FilesOp.readFile(args[0]);
     System.out.println(list);
   }
+  public List<List<Integer>> reserch(String chaineLongue, String mots[]){
+    List<List<Integer>> listOfList = new ArrayList<List<Integer>>();
+    for (String mot : mots) {
+      listOfList.add(makeIntList(chaineLongue, mot));
+    }
+    return listOfList;
+  }
+  public List<Integer> makeIntList(String s, String s2){return null;}
 }
