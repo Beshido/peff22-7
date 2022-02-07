eb1fad5db67992447fa618d6050f79c4a2162980
Aurelie Nodin <aurelie.nodin@etu.u-paris.fr>
2022-02-07 14:04:23 +0100

    Mettre les mots a rechercher dans un tableau

--- tp3/Main.java
+++ tp3/Main.java
@@ -1,8 +1,20 @@
 import java.util.List;
 
+import static java.lang.Integer.parseInt;
+
 public class Main {
   public static void main(String[] args) {
     List<String> list = FilesOp.readFile(args[0]);
     System.out.println(list);
+
+  }
+
+  public String[] motARechercher (String[] texte){
+     int n = parseInt(texte[0]);
+     String[] res = new String [n];
+     for(int i = 0; i < n; i++ ){
+       res[i] = texte[i+1];
+     }
+     return res;
   }
 }
