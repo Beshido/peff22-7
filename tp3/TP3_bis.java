5d0789903d9d0ec6ac932ef1e7512b1ec4b9e08c
Aurelie Nodin <aurelie.nodin@etu.u-paris.fr>
2022-02-11 11:59:53 +0100

    Exercice 2 fonctionne et est rapide

--- tp3/TP3_bis.java
+++ tp3/TP3_bis.java
@@ -4,40 +4,17 @@ import java.util.ArrayList;
 import static java.lang.Integer.parseInt;
 
 public class TP3_bis {
-    static String res = "";
 
   public static void main(String[] args) {
     List<String> list = FilesOp2.readFile(args[0]);
-    // System.out.println(list);
-    int nbMots = parseInt(list.get(0));
-    String resString = "";
-
-    char[] chars = makeAlphabet(motsARechercher(list));
-    int size = sizeIs(motsARechercher(list));
-    for ( int  i = 1; i < size; i++) {
-        concatene(chars, i, new char[i], 0);
-    }
-      int t [] = TP3.positionChaine(reserch(res, motsARechercher(list)));
-      int k=0;
-      for (String s : motsARechercher(list)) {
-          if(res.substring(t[1]-1).indexOf(s)==1){
-              k=s.length();
-          }
-      }
-      // System.out.println(k);
-      t[1]+=k;
-      resString = res.substring(t[0],t[1]);
-    System.out.println(resString);
-      FilesOp2.writeFile(resString,t[0],"out2");
-    res = "";
-
+      String res = result(motsARechercher(list));
+      FilesOp2.writeFile(res,res.length(),"out2");
   }
 
 
   public static String[] motsARechercher (List<String> texte){
      int n = parseInt(texte.get(0));
      String[] res = new String [n];
-     // for(int i = 0; i < n; i++ ){
      int i=0;
      for (String s : texte) {
        if(i>0 && i<=n){
@@ -47,61 +24,49 @@ public class TP3_bis {
      }
      return res;
   }
-  public static List<List<Integer>> reserch(String chaineLongue, String mots[]){
-    List<List<Integer>> listOfList = new ArrayList<List<Integer>>();
-    for (String mot : mots) {
-      listOfList.add(makeIntList(chaineLongue, mot));
-    }
-    return listOfList;
-  }
-    public static ArrayList<Integer> makeIntList(String texteSeul, String mot){
-        ArrayList<Integer> res = new ArrayList();
-        System.out.println(mot);
-        if(mot==null){return res;}
-        int a = texteSeul.indexOf(mot);
-        while(a < texteSeul.length() && a != -1){
-            res.add(a);
-            a = texteSeul.indexOf(mot, a+ mot.length());
+
+    public static String reverse(String input){
+        char[] in = input.toCharArray();
+        int begin=0;
+        int end=in.length-1;
+        char temp;
+        while(end>begin){
+            temp = in[begin];
+            in[begin]=in[end];
+            in[end] = temp;
+            end--;
+            begin++;
         }
-        return res;
+        return new String(in);
     }
 
-    public static void concatene(char[] chars, int size, char[] build, int pos){
-        if (pos == size) {
-            String word = new String (build);
-            res = res + word;
-            return;
-        }
-
-        for (char aChar : chars) {
-            build[pos] = aChar;
-            concatene(chars, size, build, pos + 1);
-        }
-    }
-
-    public static char[] makeAlphabet (String[] WordsList){
-      List<Character> listAlphabet = new ArrayList<Character>();
-      
-      for(String word : WordsList) {
-          for (int i = 0; i < word.length(); i++) {
-              if (!listAlphabet.contains(word.charAt(i))) {
-                  listAlphabet.add(word.charAt(i));
-              }
+  public static String concatene ( String mot1, String mot2) {
+      if (mot1.contains(mot2)) {
+          return mot1;
+      }
+      String fin_mot2 = "";
+      for (int i = mot2.length()-1; i > 0; i--) {
+          fin_mot2 += mot2.charAt(i);
+          String tmp = mot2.substring(0,i-1);
+          if (mot1.contains(tmp)) {
+              return mot1+reverse(fin_mot2);
           }
       }
-
-      char[] tabAlphabet = new char[listAlphabet.size()];
-      for(int i = 0; i<tabAlphabet.length;i++){
-        tabAlphabet[i] = listAlphabet.get(i);
-      }
-    return tabAlphabet;
+        return mot1+mot2;
   }
 
-  public static int sizeIs(String[] WordsList){
-      int i = 0;
-      for(String word : WordsList){
-          i = i + word.length();
+  public static String result (String[] mots){
+      for(int i = 0; i<mots.length - 1; i++){
+          String motA = mots[i];
+          String motB = mots[i+1];
+          String resultA = concatene(motA,motB);
+          String resultB = concatene(motB,motA);
+          if((resultA.length()) < (resultB.length())){
+              mots[i+1] = resultA;
+          }else{
+              mots[i+1] = resultB;
+          }
       }
-      return i;
+      return mots[mots.length -1];
   }
 }
