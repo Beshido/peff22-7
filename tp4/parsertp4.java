f34e310765fa787a60915739737fe525bf335534
Dany Hamimi <dany.hamimi@gmail.com>
2022-02-21 14:28:10 +0100

    parse

--- tp4/parsertp4.java
+++ tp4/parsertp4.java
@@ -13,38 +13,31 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.File;
 
-
 public class parsertp4 {
   //tools
-  public static List<String> readFile(String nomDuFichier){
-    List<String> gs= new ArrayList<String>();
+  public static List<String> readFile(String nomDuFichier) {
+    List<String> gs = new ArrayList<String>();
     try {
       BufferedReader lecteurAvecBuffer = null;
       String ligne;
-      String tmp = "";
       try {
         lecteurAvecBuffer = new BufferedReader(new FileReader(nomDuFichier, StandardCharsets.UTF_8));
-      } catch(FileNotFoundException e) {
-        System.out.println("Le chargement du fichier "+ nomDuFichier+" a échoué.");
+      } catch (FileNotFoundException e) {
+        System.out.println("Le chargement du fichier " + nomDuFichier + " a échoué.");
       }
       ligne = lecteurAvecBuffer.readLine();
       gs.add(ligne);
-      while ((ligne = lecteurAvecBuffer.readLine()) != null){
-        tmp = tmp + ligne;
-        if(ligne.charAt(ligne.length()-1) == '#'){
-          gs.add(tmp.substring(0,tmp.length()-1));
-          tmp = "";
-        }
-        else{
-        }
+      while ((ligne = lecteurAvecBuffer.readLine()) != null) {
+        gs.add(ligne);
       }
       lecteurAvecBuffer.close();
-    }catch (IOException e) {
+    } catch (IOException e) {
       e.printStackTrace();
     }
     return gs;
   }
-  public static boolean writeFile(String content,int t0, String nomDuFichier) {
+
+  public static boolean writeFile(String content, int t0, String nomDuFichier) {
     // nomDuFichier = str.sToDirectoryName(nomDuFichier);
     try {
       BufferedWriter ecriteurAvecBuffer = null;
@@ -53,32 +46,19 @@ public class parsertp4 {
       f.createNewFile(); //it will be crate only if it haven't been yet.
       try {
         ecriteurAvecBuffer = new BufferedWriter(new FileWriter(nomDuFichier, StandardCharsets.UTF_8));
-      } catch(FileNotFoundException e) {
+      } catch (FileNotFoundException e) {
         System.out.println("Le fichier n'as pas pu être créer. Le problème peut venir d'un caractère incorecte");
         return false;
       }
-      String a = content.length()+"\n";
+      String a = content.length() + "";
       ecriteurAvecBuffer.write(a);
-      String s = t0+"";
-      ecriteurAvecBuffer.write(s);
-      for(int i = 0 ; i<content.length();i++){
-        if(i%80 == 0){
-          ecriteurAvecBuffer.write("\n");
-        }
+      for (int i = 0; i < content.length(); i++) {
         ecriteurAvecBuffer.write(content.charAt(i));
       }
-      ecriteurAvecBuffer.write("#");
       ecriteurAvecBuffer.close();
-    }catch (IOException e) {
+    } catch (IOException e) {
       return false;
     }
     return true;
   }
-  /*public static boolean writeFile(List<String> list, String nomDuFichier){
-    String content = "";
-    for (String s : list) {
-      content+=s+"\n";
-    }
-    return writeFile(content, nomDuFichier);
-  }*/
 }
--- tp4/tp4.java
+++ tp4/tp4.java
@@ -3,6 +3,22 @@ import java.util.List;
 class tp4{
     public static void main(String[] args) {
         List<String> list = parsertp4.readFile(args[0]);
-        System.out.println(list);
+        int nbRow = Integer.parseInt((list.get(0).split(" "))[0]);
+        System.out.println("Row : "+ nbRow);
+        int nbColumns = Integer.parseInt((list.get(0).split(" "))[1]);
+        System.out.println("Columns : "+ nbColumns);
+        int nbdrones = Integer.parseInt((list.get(0).split(" "))[2]);
+        System.out.println("Row : "+ nbdrones);
+        int nbTurns = Integer.parseInt((list.get(0).split(" "))[3]);
+        System.out.println("Row : "+ nbTurns);
+        int MaxPayload = Integer.parseInt((list.get(0).split(" "))[4]);
+        System.out.println("Row : "+ MaxPayload);
+        int[] ObjectsW = new int[Integer.parseInt(list.get(1))];
+        for(int i = 0;i< ObjectsW.length;i++){
+                ObjectsW[i] = Integer.parseInt((list.get(2).split(" "))[i]);
+                System.out.println("Object numero "+i+" poids : "+ObjectsW[i]+"u");
+        }
+        
+        
     }
 }
