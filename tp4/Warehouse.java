2f49f523dcecb6308274f485efb557c7a691b3ba
lejeunea <albanlejeune94@gmail.com>
2022-02-21 14:49:36 +0100

    A

--- tp4/Warehouse.java
+++ tp4/Warehouse.java
@@ -2,7 +2,7 @@ import java.awt.*;
 import java.util.HashMap;
 
 public class Warehouse extends CarryingObjects{
-        Point location;
+        Point currentLocation;
         HashMap<Integer, Integer> listOfObject;
 
     public Warehouse(int x, int y){
