43c8a5db462daea793fb9d19c97d6d7c82cc8e94
Aurelie Nodin <aurelie.nodin@etu.u-paris.fr>
2022-02-21 15:39:40 +0100

    Carryingobjects created

---
+++ tp4/CarryingObjects.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+import java.util.HashMap;
+
+public class CarryingObjects{
+        Point currentLocation;
+        HashMap<Integer, Integer> listOfObject;
+
+    public CarryingObjects(int x, int y){
+        Point p = new Point(x,y);
+        currentLocation = p;
+        listOfObject = null;
+        }
+}
