9efc89346e4404e0c6039625710d5771adc38a38
Dany <dany@Dany.localdomain>
2022-03-03 15:43:24 +0100

    end parse

--- tp4/TP4.java
+++ tp4/TP4.java
@@ -3,11 +3,13 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 class TP4{
     public static int[] objectsWeights;
     public static Warehouse[] entrepots;
+    public static Mission[] missions;
     public static void main(String[] args) {
         List<String> list = parsertp4.readFile(args[0]);
         int nbRow = Integer.parseInt((list.get(0).split(" "))[0]);
@@ -39,13 +41,23 @@ class TP4{
 
             compteur++;
         }
-        int endLine = Integer.parseInt(list.get(3))*2+1+3;
-        for(int i = endLine+1 ; i < Integer.parseInt(list.get(endLine))*3+endLine+1;i = i+3){
-            //TODO parse Mission.
-            // System.out.println(list.get(i));
-            // System.out.println(list.get(i+1));
-            // System.out.println(list.get(i+2));
 
+
+
+        int endLine = Integer.parseInt(list.get(3))*2+1+3;
+        missions = new Mission[Integer.parseInt(list.get(endLine))];
+        compteur = 0;
+        for(int i = endLine+1 ; i < Integer.parseInt(list.get(endLine))*3+endLine+1;i = i+3){
+            missions[compteur] = new Mission(Integer.parseInt(list.get(i).split(" ")[0]), Integer.parseInt(list.get(i).split(" ")[1]));
+            //TODO parse Mission.
+            int[] objs = new int[Integer.parseInt(list.get(i+1))];
+            //int[] objs = new int[list.get(i+2).split(" ").length];
+            for(int j = 0 ; j< list.get(i+2).split(" ").length; j ++){
+                objs[j] = Integer.parseInt(list.get(i+2).split(" ")[j]);
+            }
+            missions[compteur].initializedObjects(objs);
+            System.out.println(list.get(i));
+            compteur++;
         }
         runTheDrone(nbDrones, nbTurns);
     }
@@ -78,6 +90,6 @@ class TP4{
         }
     }
     public static List<Mission> getMissions(){
-      return new ArrayList<Mission>(); //TODO
+      return Arrays.asList(missions); //TODO
     }
 }
