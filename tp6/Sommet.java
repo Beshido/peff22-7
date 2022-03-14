23093d50f67e5d56b9b5f003dce6217df39a0230
Aurelie Nodin <aurelie.nodin@etu.u-paris.fr>
2022-03-14 14:59:37 +0100

    Sommet arbitre

--- tp6/Sommet.java
+++ tp6/Sommet.java
@@ -1,10 +1,10 @@
 import java.util.HashMap;
 
 public class Sommet {
-    public static Sommet ARBIBTRE;
+    public static Sommet ARBITRE;
     public boolean estJoueur;
     public int id;
-    public HashMap<Integer, Integer> voisin;
+    public HashMap<Integer, Integer> voisins;
 
     public Sommet(boolean b, int id){
         estJoueur = b;
