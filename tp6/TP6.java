3980af838e32d659e1a1e57bc508d1afa94e8862
raveneau <emilien.raveneau@gmail.com>
2022-03-14 14:00:08 +0100

    Remove duplicate import.

--- tp6/TP6.java
+++ tp6/TP6.java
@@ -1,25 +1,22 @@
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.LinkedList;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.List;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.File;
+
 public class TP6{
     public static void main(String[] args) {
         List<String> list = parsertp6.readFile(args[0]);
