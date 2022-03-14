05d801cbf3ea06d9a09a0d7759f6b21d41d6b317
Aurelie Nodin <aurelie.nodin@etu.u-paris.fr>
2022-03-14 14:16:38 +0100

    Sommet fillHashMap

--- tp6/Sommet.java
+++ tp6/Sommet.java
@@ -3,11 +3,14 @@ import java.util.HashMap;
 public class Sommet {
     public boolean estJoueur;
     public int id;
-    public HashMap<Integer, Integer> voisin;
+    public HashMap<Integer, Integer> voisin = new HashMap<>();
 
     public Sommet(boolean b, int id){
         estJoueur = b;
         this.id = id;
-        voisin = ;
+    }
+
+    public void fillHashMap (int sommet, int poids){
+        voisin.put(sommet,poids);
     }
 }
