ccc649cb7fa0a2ba10b27a44a67477ab4477d71f
Aurelie Nodin <aurelie.nodin@etu.u-paris.fr>
2022-03-14 14:13:09 +0100

    Sommet constructeur

--- tp6/Sommet.java
+++ tp6/Sommet.java
@@ -4,4 +4,10 @@ public class Sommet {
     public boolean estJoueur;
     public int id;
     public HashMap<Integer, Integer> voisin;
+
+    public Sommet(boolean b, int id){
+        estJoueur = b;
+        this.id = id;
+        voisin = ;
+    }
 }
