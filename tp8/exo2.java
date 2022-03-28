c4e043b29fbb5c861eac12af04647e207f8f68f4
Dany Hamimi <dany.hamimi@gmail.com>
2022-03-28 15:06:18 +0200

    add for multiple file args

--- tp8/exo2.java
+++ tp8/exo2.java
@@ -8,38 +8,41 @@ public class exo2{
     private static int alen;
     private static int blen;
     public static void main(String[] args) {
-        List<String> list = parser.readFile(args[0]);
-        n = Integer.parseInt(list.get(0));
-        String as [] = list.get(1).split(" ");
-        String bs [] = list.get(2).split(" ");
-        // int a = Integer.parseInt(list.get(1));
-        // int bougieGarcon = Integer.parseInt(list.get(2));
-        a=new int[as.length];
-        b=new int[bs.length];
-        int k=0;
-        for (String s : as) {
-            a[k]=Integer.parseInt(s);
-            k++;
+
+        for(int fnum = 0; fnum < args.length;fnum++){
+            List<String> list = parser.readFile(args[fnum]);
+            n = Integer.parseInt(list.get(0));
+            String as [] = list.get(1).split(" ");
+            String bs [] = list.get(2).split(" ");
+            // int a = Integer.parseInt(list.get(1));
+            // int bougieGarcon = Integer.parseInt(list.get(2));
+            a=new int[as.length];
+            b=new int[bs.length];
+            int k=0;
+            for (String s : as) {
+                a[k]=Integer.parseInt(s);
+                k++;
+            }
+            k=0;
+            for (String s : bs) {
+                b[k]=Integer.parseInt(s);
+                k++;
+            }
+            alen=a.length;
+            blen=b.length;
+            res = new long[3];
+            res[0]=0;
+            res[1]=0;
+            res[2]=0;
+            calculateSurface();
+            String sol="";
+            for (long i : res) {
+                sol+=i+" ";
+            }
+            sol=sol.substring(0,sol.length()-1)+'\n';
+            System.out.print(sol);
+            parser.writeFile(sol, args[fnum].replaceFirst("[.][^.]+$", ".out"));
         }
-        k=0;
-        for (String s : bs) {
-            b[k]=Integer.parseInt(s);
-            k++;
-        }
-        alen=a.length;
-        blen=b.length;
-        res = new long[3];
-        res[0]=0;
-        res[1]=0;
-        res[2]=0;
-        calculateSurface();
-        String sol="";
-        for (long i : res) {
-            sol+=i+" ";
-        }
-        sol=sol.substring(0,sol.length()-1)+'\n';
-        System.out.print(sol);
-        parser.writeFile(sol, args[0].replaceFirst("[.][^.]+$", ".out"));
     }
     public static void calculateSurface(){
         for (int j=0; j<blen; j++) {
