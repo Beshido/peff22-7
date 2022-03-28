61f3d31fd44602e3f2c6de5e1e6cfa5bbec30bd0
raveneau <emilien.raveneau@gmail.com>
2022-03-28 15:24:22 +0200

    4

--- tp8/exo4.java
+++ tp8/exo4.java
@@ -1,17 +1,45 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class exo4{
 
     public static List<Integer> EquartSorted(int[] Entree, int[]Sortie){
-        List<Integer> a = new ArrayList<Integer>();
-        for(int i=0;i<Entree.length;i++){
-            if (Sortie[i]>Entree[i]){
-            a.add(Sortie[i]-Entree[i]);
+        // List<Integer> a = new ArrayList<Integer>();
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i=0; i<Entree.length; i++) {
+            for (int j=0; j<Sortie.length; j++) {
+                int x = Sortie[j]-Entree[i];
+                if (x>-1){
+                    if(map.containsKey(x)){
+                        map.put(x,map.get(x)+1);
+                    }else{
+                        map.put(x,1);
+                    }
+                }
+            }
         }
-    }
-        a.sort((Integer x, Integer y) -> y-x);
-        return a;
+        // int j=0;
+        // for(int i=0;i<Entree.length;){
+        //     if (Sortie[j]>Entree[i]){
+        //         a.add(Sortie[j]-Entree[i]);
+        //         i++;
+        //     }else{
+        //         j++;
+        //         if(j==Sortie.length){
+        //             break;
+        //         }
+        //     }
+        // }
+        // a.sort((Integer x, Integer y) -> y-x);
+        // return a;
+        System.out.println(map);
+        int maxI=0;
+        for (Integer i : map.keySet()) {
+            if(map.get(i)>map.get(maxI)){
+                maxI=i;
+            }
+        }
+        System.out.println(maxI);
+        return null;
 
     }
     public static int calculEquart(List<Integer>tabEquart){
@@ -19,18 +47,18 @@ public class exo4{
         int count = 0;
         int val =tabEquart.get(0);
         int trucARetenir=0;
-        
+
         for(int i=0;i<tabEquart.size();i++){
             if(val!=tabEquart.get(i)){
-            val = tabEquart.get(i);
-            count = 1;
+                val = tabEquart.get(i);
+                count = 1;
             }
             if(max< count){
-            max=count;
-            trucARetenir=tabEquart.get(i);
+                max=count;
+                trucARetenir=tabEquart.get(i);
             }
-            else count++;   
-            
+            else count++;
+
         }
     return trucARetenir;
 }
@@ -51,7 +79,7 @@ public class exo4{
         for(int i = 0;i<M;i++){
             MtempsTab[i] = Integer.parseInt(Mtemps.split(" ")[i]);
         }
-        
-        System.out.println(calculEquart(EquartSorted(NtempsTab, MtempsTab)));  
+
+        System.out.println(calculEquart(EquartSorted(NtempsTab, MtempsTab)));
     }
 }
