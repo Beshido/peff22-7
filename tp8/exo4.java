5771aeac3f4b648a630c345c51ee2936ec648607
lejeunea <albanlejeune94@gmail.com>
2022-03-28 14:17:52 +0200

    EXO 4 v1

--- tp8/exo4.java
+++ tp8/exo4.java
@@ -1,5 +1,30 @@
 public class exo4{
+
+    public static int[] EquartSorted(int[] Entree, int[]Sortie){
+        int[]tab= new int [Entree.length];
+        for(int i=0;i<Entree.length;i++){
+            tab[i]=(Sortie[i]-Entree[i]);
+        }
+        java.util.Arrays.sort(tab);
+        return tab;
+
+    }
+    public static int calculEquart(int[]tabEquart){
+        int max = 0;
+        int count = 0;
+        for (int i = 0; i < tabEquart.length; i++) {
+            int num = tabEquart[i];
+        if (num == max) {
+            count++;
+        } else if (num > max) {
+        max = num;
+        count = 1;
+        }
+    }return max;
+}
+
     public static void main(String[] args) {
+
         
     }
 }
