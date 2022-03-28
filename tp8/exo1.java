c3b182fedb76afe3b6b117500f4ae1ac18b8ceb0
Dany Hamimi <dany.hamimi@gmail.com>
2022-03-28 13:52:10 +0200

    exo1 parsed

--- tp8/exo1.java
+++ tp8/exo1.java
@@ -3,5 +3,13 @@ import java.util.List;
 public class exo1{
     public static void main(String[] args) {
         List<String> list = parser.readFile(args[0]);
+        int largeur = Integer.parseInt(list.get(0));
+        int nbPieces = Integer.parseInt(list.get(1));
+        int[][] pieces = new int[nbPieces+2][2];
+        for(int i = 2; i < nbPieces+2; i++){
+            String[] tab = list.get(i).split(" ");
+            pieces[i][0] = Integer.parseInt(tab[0]);
+            pieces[i][1] = Integer.parseInt(tab[1]);
+        }
     }
 }
