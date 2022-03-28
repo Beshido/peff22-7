fd02ac6ae9d12aaca2873b10148937bffe3c8058
raveneau <emilien.raveneau@gmail.com>
2022-03-28 14:06:29 +0200

    exo2 working.

--- tp8/exo2.java
+++ tp8/exo2.java
@@ -1,12 +1,48 @@
 import java.util.List;
 
 public class exo2{
+    private static int n;
+    private static int a[];
+    private static int b[];
+    private static long res[];
+    private static int alen;
+    private static int blen;
     public static void main(String[] args) {
         List<String> list = parser.readFile(args[0]);
-        int ecart = Integer.parseInt(list.get(0));
-        int bougieFille = Integer.parseInt(list.get(1));
-        int bougieGarcon = Integer.parseInt(list.get(2));
-        
-        
+        n = Integer.parseInt(list.get(0));
+        String as [] = list.get(1).split(" ");
+        String bs [] = list.get(2).split(" ");
+        // int a = Integer.parseInt(list.get(1));
+        // int bougieGarcon = Integer.parseInt(list.get(2));
+        a=new int[as.length];
+        b=new int[bs.length];
+        int k=0;
+        for (String s : as) {
+            a[k]=Integer.parseInt(s);
+            k++;
+        }
+        k=0;
+        for (String s : bs) {
+            b[k]=Integer.parseInt(s);
+            k++;
+        }
+        alen=a.length;
+        blen=b.length;
+        res = new long[3];
+        res[0]=0;
+        res[1]=0;
+        res[2]=0;
+        calculateSurface();
+        for (long i : res) {
+            System.out.print(i+" ");
+        }
+        System.out.println();
+    }
+    public static void calculateSurface(){
+        for (int j=0; j<blen; j++) {
+            for (int i=0; i<alen; i++) {
+                res[(i+j+2)%3]+=a[i]*b[j];
+            }
+        }
     }
 }
