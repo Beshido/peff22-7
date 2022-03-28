3a112d8590220e73ad9cd9ff42f38ce6428ff69c
HAMIMI dany <dany.hamimi@gmail.com>
2022-03-28 14:12:59 +0200

    Update tp8/exo1.java

--- tp8/exo1.java
+++ tp8/exo1.java
@@ -1,7 +1,13 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
 public class exo1{
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        int sommeglo = 0;
+        String filename = args[0];
+        filename = filename.substring(0, filename.lastIndexOf('.'));
         List<String> list = parser.readFile(args[0]);
         int largeur = Integer.parseInt(list.get(0));
         int nbPieces = Integer.parseInt(list.get(1));
@@ -10,6 +16,20 @@ public class exo1{
             String[] tab = list.get(i).split(" ");
             pieces[i][0] = Integer.parseInt(tab[0]);
             pieces[i][1] = Integer.parseInt(tab[1]);
+            sommeglo = sommeglo + Integer.parseInt(tab[0]) * Integer.parseInt(tab[1]);
         }
+        Integer a = sommeglo/largeur;
+        String k = a.toString();
+        outWrite(filename,k);
+        System.out.println(sommeglo/largeur);
+        //        write in out file the solution
+        
+        
     }
+
+    public static void outWrite(String str1, String str2) throws IOException {
+    BufferedWriter writer = new BufferedWriter(new FileWriter(str1+".out"));
+    writer.write(str2);
+    writer.close();
+}
 }
