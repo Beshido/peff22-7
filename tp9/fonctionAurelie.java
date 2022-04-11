53ad1a474798978d66496149826e6cee84021985
Aurelie Nodin <aurelie.nodin@etu.u-paris.fr>
2022-04-11 15:27:35 +0200

    fonction aurelie reparation iscolorline

--- tp9/fonctionAurelie.java
+++ tp9/fonctionAurelie.java
@@ -5,18 +5,18 @@ public class fonctionAurelie {
         for(int i = 0; i< dominos.length; i++) {
             if (dominos[i] != null) {
                 if (dominos[i].couleur == color) {
-                    return false;
+                    return true;
                 }
             }
         }
-        return true;
+        return false;
     }
 
     public static int[][] oneDomManyCol (List<Domino> dominos, int l){
         Domino[][] res = new Domino[l][dominos.size()];
         int i = l-1;
         int j = 0;
-        while((!dominos.isEmpty()) && i>0 && (j < dominos.size()-1)){
+        while((!dominos.isEmpty()) && i>0 ){
             for(Domino domino : dominos){
                 if(!colorInLine(res[i],domino.couleur)){
                     res[i][j] = domino;
@@ -24,8 +24,11 @@ public class fonctionAurelie {
                     break;
                 }
             }
-            i--;
-            j++;
+            if(j == dominos.size()) {
+                i--;
+            }else {
+                j++;
+            }
         }
         int[][] res2 = TP9.toDominoId(res);
         return res2;
