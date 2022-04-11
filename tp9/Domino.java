cf8942cc19346906ccfdb47003cc2756651b3da4
Dany Hamimi <dany.hamimi@gmail.com>
2022-04-11 13:53:05 +0200

    Domino object

---
+++ tp9/Domino.java
@@ -0,0 +1,13 @@
+public class Domino {
+    public int haut;
+    public int bas;
+    public int gauche;
+    public int droite;
+    
+    public Domino(int haut, int bas, int gauche, int droite) {
+        this.haut = haut;
+        this.bas = bas;
+        this.gauche = gauche;
+        this.droite = droite;
+    }
+}
---
+++ tp9/TP9.java
@@ -0,0 +1,5 @@
+public class TP9{
+    public static void main(String[] args) {
+        
+    }
+}
