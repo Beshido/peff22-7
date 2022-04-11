0d76d8475649379f5beecbd9e1228c3a516bc58b
Dany Hamimi <dany.hamimi@gmail.com>
2022-04-11 13:53:37 +0200

    Domino object

--- tp9/Domino.java
+++ tp9/Domino.java
@@ -3,11 +3,13 @@ public class Domino {
     public int bas;
     public int gauche;
     public int droite;
+    public int couleur;
     
-    public Domino(int haut, int bas, int gauche, int droite) {
+    public Domino(int haut, int bas, int gauche, int droite, int couleur) {
         this.haut = haut;
         this.bas = bas;
         this.gauche = gauche;
         this.droite = droite;
+        this.couleur = couleur;
     }
 }
