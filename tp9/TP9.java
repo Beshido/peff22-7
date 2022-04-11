68c83c7598f2fbae72298a70c1db44f3270e0d9d
raveneau <emilien.raveneau@gmail.com>
2022-04-11 14:14:20 +0200

    2 help fct

--- tp9/TP9.java
+++ tp9/TP9.java
@@ -1,5 +1,7 @@
 import java.util.LinkedList;
 import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TP9{
     public static void main(String[] args) {
@@ -15,7 +17,31 @@ public class TP9{
             Domino d = new Domino(Integer.parseInt(tab[0]), Integer.parseInt(tab[1]), Integer.parseInt(tab[2]), Integer.parseInt(tab[3]), Integer.parseInt(tab[4]), i);
             dominoes.add(d);
         }
-        
 
+
+    }
+    public static Map<Integer, Integer> colorInfo(List<Domino> dominos){
+        Map<Integer, Integer> colors = new HashMap<Integer, Integer>();
+        for (Domino d : dominos) {
+            if(colors.containsKey(d.couleur)){
+                colors.put(d.couleur, colors.get(d.couleur)+1);
+            }else{
+                colors.put(d.couleur, 1);
+            }
+        }
+        return colors;
+    }
+    public static Map<Integer, Integer> numberInfo(List<Domino> dominos){
+        Map<Integer, Integer> numbers = new HashMap<Integer, Integer>();
+        for (Domino d : dominos) {
+            for (int side : d.getSide()) {
+                if(numbers.containsKey(side)){
+                    numbers.put(side, numbers.get(side)+1);
+                }else{
+                    numbers.put(side, 1);
+                }
+            }
+        }
+        return numbers;
     }
 }
